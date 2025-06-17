84df52fc1a4565f75ad5614c3ac17aab957cb8cd
Vadim Popov <m.vadimpopov@gmail.com>
2025-06-16 21:55:26 -0500

    feat(MVP): jwt auth (#3)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService,
+    ) { }
 
     async signin(dto: AuthDto) {
         // find the user by email
@@ -20,18 +26,19 @@ export class AuthService {
 
         // compare password
         const pwMatches = await argon.verify(user.hash, dto.password)
+
         // if password incorrect throw exception
         if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
 
-        // send back the user
-        const { hash, ...userWithoutHash } = user;
-        return userWithoutHash;
+        // return the JWT token 
+        return this.signToken(user.id, user.email)
     }
 
     async signup(dto: AuthDto) {
         try {
             // generate the password hash
             const hash = await argon.hash(dto.password);
+
             // save the new user in the db
             const user = await this.prisma.user.create({
                 data: {
@@ -45,8 +52,8 @@ export class AuthService {
                 }
             })
 
-            // return the saved user
-            return user;
+            // return the JWT token 
+            return this.signToken(user.id, user.email)
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code == 'P2002') {
@@ -54,6 +61,27 @@ export class AuthService {
                 }
             }
             throw error.code; // rethrow the error if it's not a known Prisma error
+        }
+    }
+
+    async signToken(
+        userId: number,
+        email: string
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email,
+        }
+
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret,
+        })
+
+        return {
+            access_token: token
         }
     }
 
